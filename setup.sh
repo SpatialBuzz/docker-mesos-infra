71df672674be970d40340a067d5ab4f5f62329bc
Andrew Blake <andrew.blake@spatialbuzz.com>
2015-04-10 12:01:11 +0100

    Add docker build steps

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 vagrant up registry
+
+pushd Docker/Nginx
+docker build -t 192.168.1.119:5000/nginx .
+docker push 192.168.1.119:5000/nginx
+popd
+
+pushd Docker/App  
+docker build -t 192.168.1.119:5000/app .  
+docker push 192.168.1.119:5000/app  
+popd
+
 vagrant up lb1 lb2
 vagrant up mm1 mm2 mm3
 vagrant up ms1
